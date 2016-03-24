b87c1b15153bed21646e301c2d38f4b1bf5fb900
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2016-03-24 13:39:05 +0100

    http-kernel controller-resolver

--- src/app.php
+++ src/app.php
@@ -12,6 +12,19 @@ function is_leap_year($year = null)
     return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
 }
 
+class LeapYearController
+{
+    public function indexAction($year)
+    {
+        if(is_leap_year($year)){
+            return new Response('Yep, this is a leap year');
+        }
+
+        return new Response('Nope, this is not a leap year');
+    }
+
+}
+
 $routes = new Routing\RouteCollection();
 $routes->add(
     'hello',
@@ -39,13 +52,7 @@ $routes->add(
     new Routing\Route('/is_leap_year/{year}',
         array(
             'year' => null,
-            '_controller' => function($request){
-                if(is_leap_year($request->attributes->get('year'))){
-                    return new Response('Yep, this is a leap year!');
-                }
-
-                return new Response('Nope, this is not a leap year.');
-            }
+            '_controller' => 'LeapYearController::indexAction'
         )
     )
 );

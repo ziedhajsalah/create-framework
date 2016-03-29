f325803a6783b9c3cc6d0b54e0c6be9fe5d1442d
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2016-03-29 10:40:49 +0100

    dependency-injection

--- web/front.php
+++ web/front.php
@@ -3,35 +3,12 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Simplex\GoogleListener;
-use Simplex\ContentLengthListener;
 
-function render_template(Request $request)
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
+$routes = include __DIR__ . '/../src/app.php';
+$sc = include __DIR__ . '/../src/container.php';
 
 $request = Request::createFromGlobals();
-$routes = include __DIR__.'/../src/app.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$resolver = new HttpKernel\Controller\ControllerResolver();
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
-
-$framework = new \Simplex\Framework($dispatcher, $resolver);
-// $framework = new HttpKernel\HttpCache\HttpCache($framework, new HttpKernel\HttpCache\Store(__DIR__ . '/../cache'));
-$response = $framework->handle($request);
+$response = $sc->get('framework')->handle($request);
 
 $response->send();
